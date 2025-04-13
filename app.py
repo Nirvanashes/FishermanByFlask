0674443e2f28352dca4544abf8af18c9bca34566
LvJiaHao <23491399+Nirvanashes@users.noreply.github.com>
2025-04-13 14:16:58 +0800

    初始化项目

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap5
+from flask_ckeditor import CKEditor
+from flask_gravatar import Gravatar
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from flask_mail import Mail
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, EmailField
+from wtforms.validators import DataRequired, URL
 
 app = Flask(__name__)
 
